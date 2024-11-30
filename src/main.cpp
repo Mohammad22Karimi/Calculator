45987c9aea625f7316be7e0f7220ffb82806d58d
Karimi <mokarimi22.4@gmail.com>
2024-11-30 17:46:51 +0330

    Test Stack class functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include "Stack.cpp"
 using namespace std;
 
 int main()
 {
+    // testing Stack:
+    Stack stack(5);
+    try
+    {
+        stack.push('a');
+        stack.push('b');
+        stack.push('c');
+
+        cout << "top element: " << stack.peek() << endl;
+        stack.pop();
+        cout << "top element after pop: " << stack.peek() << endl;
+    }
+    catch (const std::exception &e)
+    {
+        cerr << e.what() << '\n';
+    }
 
     return 0;
 }
