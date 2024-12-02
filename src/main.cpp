9d835ad9eadd3d08ff9fca53edeed937cfee27a3
Karimi <mokarimi22.4@gmail.com>
2024-12-02 17:15:38 +0330

    deleting the code of "test the stack" and adding infixToPostfix function and test that

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,63 @@
 #include <iostream>
+#include <string>
+#include <cctype> // for checking numbers and variables
 #include "Stack.cpp"
+#include "Utils.cpp"
 using namespace std;
+
+string infixToPostfix(const string &infix)
+{
+    Stack stack(infix.length()); // stack for holding operators
+    string postfix = "";
+    for (char c : infix)
+    {
+        if (isalnum(c)) // if the character is a number or variable
+        {
+            postfix += c;
+        }
+        else if (c == '(')
+        {
+            stack.push(c);
+        }
+        else if (c == ')')
+        {
+            while (!stack.isEmpty() && stack.peek() != '(')
+            {
+                postfix += stack.pop();
+            }
+            stack.pop(); // delete the (
+        }
+        else // if it was operator
+        {
+            while (!stack.isEmpty() && precedence(stack.peek()) >= precedence(c))
+            {
+                postfix += stack.pop();
+            }
+            stack.push(c);
+        }
+    }
+
+    // add remaining operators on the stack to the postfix
+    while (!stack.isEmpty())
+    {
+        postfix += stack.pop();
+    }
+    return postfix;
+}
 
 int main()
 {
-    // testing Stack:
-    Stack stack(5);
+    string in;
+    cin >> in;
+
     try
     {
-        stack.push('a');
-        stack.push('b');
-        stack.push('c');
-
-        cout << "top element: " << stack.peek() << endl;
-        stack.pop();
-        cout << "top element after pop: " << stack.peek() << endl;
+        string postfix = infixToPostfix(in);
+        cout << "postfix: " << postfix << endl;
     }
     catch (const std::exception &e)
     {
-        cerr << e.what() << '\n';
+        std::cerr << e.what() << '\n';
     }
 
     return 0;
